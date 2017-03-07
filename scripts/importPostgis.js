ae222c0878dde2d390d867322b10b9e510b33d86
Hannes Junnila <hannes.junnila@gmail.com>
2017-03-07 15:40:17 +0200

    add importing data

--- scripts/importPostgis.js
+++ scripts/importPostgis.js
@@ -1,207 +1,231 @@
-const knex = require('knex')({ dialect: 'postgres' });
+const knex = require("knex")({
+  dialect: "postgres",
+  client: "pg",
+  connection: process.env.PG_CONNECTION_STRING
+});
 
 // install postgis functions in knex.postgis;
-const st = require('knex-postgis')(knex);
+const st = require("knex-postgis")(knex);
 
-const _ = require('lodash');
+const _ = require("lodash");
+const path = require("path");
+
+const parseDat = require("./parseDat");
+
+const SRC_PATH = "../data/src";
+
+const sourcePath = filename => path.join(__dirname, SRC_PATH, filename);
 
 const tables = {
   stops: {
-    filename: 'pysakki.dat',
+    filename: "pysakki.dat",
     fields: [
       {
         length: 7,
-        name: 'stopId',
-        type: 'string',
+        name: "stopId",
+        type: "string",
         unique: true,
         primary: true,
         index: true
       },
       { length: 7 },
       { length: 7 },
-      { length: 8, name: 'lat', type: 'decimal' },
-      { length: 8, name: 'lon', type: 'decimal' },
-      { length: 20, name: 'name_fi', type: 'string' },
-      { length: 20, name: 'name_se', type: 'string' },
-      { length: 20, name: 'address_fi', type: 'string' },
-      { length: 20, name: 'address_se', type: 'string' },
-      { length: 3, name: 'platform', type: 'string' },
+      { length: 8, name: "lat", type: "decimal" },
+      { length: 8, name: "lon", type: "decimal" },
+      { length: 20, name: "name_fi", type: "string" },
+      { length: 20, name: "name_se", type: "string" },
+      { length: 20, name: "address_fi", type: "string" },
+      { length: 20, name: "address_se", type: "string" },
+      { length: 3, name: "platform", type: "string" },
       { length: 7 },
       { length: 7 },
       { length: 20 },
       { length: 20 },
       { length: 2 },
-      { length: 6, name: 'shortId', type: 'string' },
+      { length: 6, name: "shortId", type: "string" },
       { length: 8 },
       { length: 8 },
       { length: 1 },
-      { length: 15, name: 'heading', type: 'string' },
+      { length: 16, name: "heading", type: "string" },
       { length: 3 },
       {
         length: 7,
-        name: 'terminalId',
-        type: 'string',
-        references: 'terminals.terminalId'
+        name: "terminalId",
+        type: "string"
       },
       {
         length: 7,
-        name: 'stopAreaId',
-        type: 'string',
-        references: 'stopAreas.stopAreaId'
+        name: "stopAreaId",
+        type: "string",
+        references: "stopAreas.stopAreaId"
       }
     ]
   },
   terminals: {
-    filename: 'terminaali.dat',
+    filename: "terminaali.dat",
     fields: [
       {
         length: 7,
-        name: 'terminalId',
-        type: 'string',
+        name: "terminalId",
+        type: "string",
         unique: true,
         primary: true,
         index: true
       },
-      { length: 40, name: 'name_fi', type: 'string' },
-      { length: 40, name: 'name_se', type: 'string' },
+      { length: 40, name: "name_fi", type: "string" },
+      { length: 40, name: "name_se", type: "string" },
       { length: 14 },
-      { length: 8, name: 'lat', type: 'decimal' },
-      { length: 8, name: 'lon', type: 'decimal' }
+      { length: 8, name: "lat", type: "decimal" },
+      { length: 8, name: "lon", type: "decimal" }
     ]
   },
   stopAreas: {
-    filename: 'pysakkialue.dat',
+    filename: "pysakkialue.dat",
     fields: [
       {
         length: 6,
-        name: 'stopAreaId',
-        type: 'string',
+        name: "stopAreaId",
+        type: "string",
         unique: true,
         primary: true,
         index: true
       },
-      { length: 40, name: 'name_fi', type: 'string' },
-      { length: 40, name: 'name_se', type: 'string' },
+      { length: 40, name: "name_fi", type: "string" },
+      { length: 40, name: "name_se", type: "string" },
       { length: 14 },
-      { length: 8, name: 'lat', type: 'decimal' },
-      { length: 8, name: 'lon', type: 'decimal' }
+      { length: 8, name: "lat", type: "decimal" },
+      { length: 8, name: "lon", type: "decimal" }
     ]
   },
   lines: {
-    filename: 'linjanimet2.dat',
+    filename: "linjannimet2.dat",
     fields: [
       {
         length: 6,
-        name: 'lineId',
-        type: 'string',
+        name: "lineId",
+        type: "string",
         unique: true,
         primary: true,
         index: true
       },
-      { length: 60, name: 'name_fi', type: 'string' },
-      { length: 60, name: 'name_se', type: 'string' },
-      { length: 30, name: 'origin_fi', type: 'string' },
-      { length: 30, name: 'origin_se', type: 'string' },
-      { length: 30, name: 'destination_fi', type: 'string' },
-      { length: 30, name: 'destination_se', type: 'string' }
+      { length: 60, name: "name_fi", type: "string" },
+      { length: 60, name: "name_se", type: "string" },
+      { length: 30, name: "origin_fi", type: "string" },
+      { length: 30, name: "origin_se", type: "string" },
+      { length: 30, name: "destination_fi", type: "string" },
+      { length: 30, name: "destination_se", type: "string" }
     ]
   },
   routes: {
-    filename: 'linja3',
+    filename: "linja3.dat",
     fields: [
       {
         length: 6,
-        name: 'routeId',
-        type: 'string',
-        primary: ['routeId', 'dateBegin', 'dateEnd', 'direction'],
+        name: "routeId",
+        type: "string",
         index: true
       },
-      { length: 8, name: 'dateBegin', type: 'date' },
-      { length: 8, name: 'dateEnd', type: 'date' },
-      { length: 1, name: 'direction', type: 'string' },
+      { length: 8, name: "dateBegin", type: "date" },
+      { length: 8, name: "dateEnd", type: "date" },
+      { length: 1, name: "direction", type: "string" },
       { length: 60 },
       { length: 60 },
-      { length: 2, name: 'type', type: 'string' },
+      { length: 2, name: "type", type: "string" },
       { length: 20 },
       { length: 20 },
       { length: 7 },
       { length: 5 },
-      { length: 20, name: 'destination_fi', type: 'string' },
-      { length: 20, name: 'destination_se', type: 'string' }
+      { length: 20, name: "destination_fi", type: "string" },
+      { length: 20, name: "destination_se", type: "string" }
     ]
   },
   routeSegments: {
-    filename: 'reitti.dat',
+    filename: "reitti.dat",
     fields: [
-      { length: 7, name: 'stopId', type: 'string', references: 'stops.stopId' },
+      { length: 7, name: "stopId", type: "string", references: "stops.stopId" },
       { length: 7 },
       {
         length: 6,
-        name: 'routeId',
-        type: 'string',
-        primary: ['routeId', 'dateBegin', 'dateEnd', 'direction'],
+        name: "routeId",
+        type: "string",
         index: true
       },
-      { length: 1, name: 'direction', type: 'string' },
-      { length: 8, name: 'dateBegin', type: 'date' },
-      { length: 8, name: 'dateEnd', type: 'date' },
+      { length: 1, name: "direction", type: "string" },
+      { length: 8, name: "dateBegin", type: "date" },
+      { length: 8, name: "dateEnd", type: "date" },
       { length: 20 },
-      { length: 3, name: 'duration', type: 'integer' },
-      { length: 3, name: 'stopNumber', type: 'integer' },
+      { length: 3, name: "duration", type: "integer" },
+      { length: 3, name: "stopNumber", type: "integer" },
       { length: 94 },
-      { length: 1, name: 'timingStopType', type: 'integer' }
+      { length: 1, name: "timingStopType", type: "integer" }
     ]
   },
   geometries: {
-    filename: 'reittimuoto.dat',
+    filename: "reittimuoto.dat",
     fields: [
       {
         length: 6,
-        name: 'routeId',
-        type: 'string',
-        primary: ['routeId', 'dateBegin', 'dateEnd', 'direction'],
+        name: "routeId",
+        type: "string",
         index: true
       },
-      { length: 1, name: 'direction', type: 'string' },
-      { length: 8, name: 'beginDate', type: 'date' },
-      { length: 8, name: 'endDate', type: 'date' },
+      { length: 1, name: "direction", type: "string" },
+      { length: 8, name: "beginDate", type: "date" },
+      { length: 8, name: "endDate", type: "date" },
       { length: 7 },
       { length: 1 },
       { length: 4 },
-      { length: 7, name: 'lon', type: 'integer' },
-      { length: 7, name: 'lat', type: 'integer' }
+      { length: 7, name: "y", type: "integer" },
+      { length: 7, name: "x", type: "integer" }
     ]
   },
   timetables: {
-    filename: 'aikat.dat',
+    filename: "aikat.dat",
     fields: [
-      { length: 7, name: 'stopId', type: 'string', references: 'stops.stopId' },
       {
-        length: 6,
-        name: 'routeId',
-        type: 'string',
-        primary: ['routeId', 'dateBegin', 'dateEnd', 'direction'],
+        length: 7,
+        name: "stopId",
+        type: "string",
+        references: "stops.stopId",
         index: true
       },
-      { length: 1, name: 'direction', type: 'string' },
-      { length: 2, name: 'dayType', type: 'string' },
+      {
+        length: 6,
+        name: "routeId",
+        type: "string",
+        index: true
+      },
+      { length: 1, name: "direction", type: "string" },
+      { length: 2, name: "dayType", type: "string" },
       { length: 4, name: null },
       { length: 1, name: null },
-      { length: 2, name: 'hours', type: 'integer' },
-      { length: 2, name: 'minutes', type: 'integer' },
-      { length: 1, name: 'isAccessible', type: 'integer' },
-      { length: 8, name: 'dateBegin', type: 'date' },
-      { length: 8, name: 'dateEnd', type: 'date' }
+      { length: 2, name: "hours", type: "integer" },
+      { length: 2, name: "minutes", type: "integer" },
+      { length: 1, name: "isAccessible", type: "integer" },
+      { length: 8, name: "dateBegin", type: "date" },
+      { length: 8, name: "dateEnd", type: "date" }
     ]
   }
 };
 
-let schema = knex.schema;
-
-Object.entries(tables).forEach(function([tableName, { fields }]) {
+function dropTables(schema) {
   schema = schema
-    .dropTableIfExists(tableName)
-    .createTable(tableName, function(table) {
+    .dropTableIfExists("timetables")
+    .dropTableIfExists("routeSegments")
+    .dropTableIfExists("stops")
+    .dropTableIfExists("terminals")
+    .dropTableIfExists("stopAreas");
+
+  Object.keys(tables).forEach(function(tableName) {
+    schema = schema.dropTableIfExists(tableName);
+  });
+
+  return schema;
+}
+
+function createTables(schema) {
+  Object.entries(tables).forEach(function([tableName, { fields }]) {
+    schema = schema.createTable(tableName, function(table) {
       fields.forEach(function({
         length,
         name,
@@ -213,8 +237,10 @@ Object.entries(tables).forEach(function([tableName, { fields }]) {
       }) {
         if (name && type) {
           let column;
-          if (type === 'string') {
+          if (type === "string") {
             column = table.string(name, length);
+          } else if (type === "decimal") {
+            column = table.decimal(name, 9, 6);
           } else {
             column = table[type](name);
           }
@@ -231,15 +257,59 @@ Object.entries(tables).forEach(function([tableName, { fields }]) {
           if (index) {
             column = column.index();
           }
-          if (references) {
-            column = column.references(references);
-          }
         }
       });
-      if (_.find(fields, { name: 'lat' }) && _.find(fields, { name: 'lon' })) {
-        table.specificType('point', 'geometry(point, 4326)');
+      if (
+        (_.find(fields, { name: "lat" }) && _.find(fields, { name: "lon" })) ||
+        (_.find(fields, { name: "x" }) && _.find(fields, { name: "y" }))
+      ) {
+        table.specificType("point", "geometry(point, 4326)");
       }
     });
-});
+  });
 
-console.log(schema.toString());
+  return schema;
+}
+
+function createForeignKeys(schema) {
+  Object.entries(tables).forEach(function([tableName, { fields }]) {
+    schema = schema.table(tableName, function(table) {
+      fields.forEach(function({ name, type, references }) {
+        if (name && type && references) {
+          table.foreign(name).references(references);
+        }
+      });
+    });
+  });
+
+  return schema;
+}
+
+knex.transaction(function(trx) {
+  function loadTable(tableName) {
+    return parseDat(
+      sourcePath(tables[tableName].filename),
+      tables[tableName].fields,
+      knex, tableName, trx
+    );
+  }
+
+  function loadData() {
+    return loadTable("terminals")
+      .then(() => loadTable("stopAreas"))
+      .then(() => loadTable("stops"))
+      .then(() => Promise.all([
+        loadTable("lines"),
+        loadTable("routes"),
+        loadTable("routeSegments"),
+        loadTable("geometries"),
+        loadTable("timetables")
+      ]))
+  }
+
+  createForeignKeys(createTables(dropTables(trx.schema)))
+    .then(loadData)
+    .then(trx.commit)
+    .then(res => console.log(res), trx.rollback)
+    .then(() => knex.destroy());
+});
--- scripts/parseDat.js
+++ scripts/parseDat.js
@@ -3,37 +3,61 @@ var path = require("path");
 var readline = require("readline");
 var iconv = require("iconv-lite");
 
+const whitespaceTest = /^\s+$/;
 
 function parseLine(line, fields) {
-    const stop = {};
-    let index = 1;
-    fields.forEach(([length, key, isNumeric]) => {
-        if(key) {
-            const value = line.substring(index, index + length).trim();
-            stop[key] = isNumeric ? parseFloat(value) : value;
+  const stop = {};
+  let index = 1;
+  fields.forEach(({ length, name, type }) => {
+    if (name) {
+      const value = line.substring(index, index + length).trim();
+      if (type === "decimal") {
+        stop[name] = parseFloat(value);
+      } else if (type === "date") {
+        if (value.length !== 8) {
+          console.log(line);
         }
-        index = index + length;
-    });
-    return stop;
+        stop[
+          name
+        ] = `${value.slice(0, 4)}-${value.slice(4, 6)}-${value.slice(6, 8)}`;
+      } else {
+        stop[name] = value;
+      }
+    }
+    index = index + length;
+  });
+  return stop;
 }
 
-function parseDat(filename, fields) {
-    const records = [];
+function parseDat(filename, fields, knex, tableName, trx) {
+  let i = 0;
+  const promises = []
+  let results = []
 
-    return new Promise((resolve) => {
-        const lineReader = readline.createInterface({
-            input: fs.createReadStream(filename)
-                .pipe(iconv.decodeStream("ISO-8859-15"))
-        });
-
-        lineReader.on("line", (line) => {
-            records.push(parseLine(line, fields));
-        });
-
-        lineReader.on("close", (line) => {
-            resolve(records);
-        });
+  return new Promise(resolve => {
+    const lineReader = readline.createInterface({
+      input: fs
+        .createReadStream(filename)
+        .pipe(iconv.decodeStream("ISO-8859-15"))
     });
+
+    lineReader.on("line", line => {
+      if (!whitespaceTest.test(line)) {
+        results.push(parseLine(line, fields))
+        if (++i % 2000 === 0) {
+          promises.push(knex(tableName).transacting(trx).insert(results));
+          results = []
+          console.log(`${filename} ${i}`);
+        }
+      }
+    });
+
+    lineReader.on("close", line => {
+      promises.push(knex(tableName).transacting(trx).insert(results));
+      console.log(`${filename} ${i}`);
+      resolve(Promise.all(promises));
+    });
+  });
 }
 
 module.exports = parseDat;
