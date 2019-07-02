5b499d4d1882dd9b5eb307ac8501d88262abfcb8
Daniel Dunderfelt <daniel@developsuperpowers.com>
2019-07-02 09:27:24 +0300

    Fix faulty sql script path.

--- src/setup/clearDb.js
+++ src/setup/clearDb.js
@@ -5,6 +5,6 @@ import { getKnex } from "../knex";
 const { knex } = getKnex();
 
 export async function clearDb() {
-  const dropSchema = await fs.readFile(path.join(__dirname, "clearDb.sql"), "utf8");
+  const dropSchema = await fs.readFile(path.join(__dirname, "dropSchema.sql"), "utf8");
   return knex.raw(dropSchema);
 }
