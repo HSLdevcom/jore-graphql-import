afabb9d69b4245608e7092105cdbd6845ad0c4d4
Juho Hänninen <juho.hanninen@cgi.com>
2023-09-07 08:29:30 +0300

    Upgrade some packages and remove extra dependecis from streams

--- src/database.js
+++ src/database.js
@@ -1,27 +1,26 @@
 import schema from "./schema";
 import { parseLine } from "./parseLine";
 import { map, collect } from "etl";
-import throughConcurrent from "through2-concurrent";
 import { upsert } from "./utils/upsert";
 import { getIndexForTable } from "./utils/getIndexForTable";
 import { createPrimaryKey } from "./utils/createPrimaryKey";
 import { uniqBy } from "lodash";
 import { getPrimaryConstraint } from "./utils/getPrimaryConstraint";
 import { getKnex } from "./knex";
+import { Transform } from "stream";
 
 const { knex } = getKnex();
 const NS_PER_SEC = 1e9;
 
 const createImportQuery = (insertOptions) =>
-  new Promise(async (resolve, reject) => {
+  new Promise((resolve, reject) => {
     const time = process.hrtime();
-
-    try {
-      await knex.transaction((trx) => upsert({ ...insertOptions, trx }));
-      resolve(time);
-    } catch (err) {
-      reject(err);
-    }
+    knex
+      .transaction((trx) => upsert({ ...insertOptions, trx }))
+      .then(() => {
+        resolve(time);
+      })
+      .catch((err) => reject(err));
   });
 
 const hasProhibitedNulls = (parsedLine, lineSchema) => {
@@ -47,34 +46,37 @@ const createLineParser = (tableName) => {
   const { fields, lineSchema = fields } = schema[tableName] || {};
   let linesReceived = false;
 
-  return throughConcurrent.obj({ maxConcurrency: 100 }, (line, enc, cb) => {
-    if (!line) {
-      // line === null marks the end of the file. End the import stream
-      // to flush any items left in the collect buffer.
-      return cb(null, null);
-    }
-
-    if (lineSchema) {
-      // This function runs on each line which would be too much to log.
-      // When receiving the first line of a table, log it and mark it as logged.
-      if (!linesReceived) {
-        console.log(`Importing ${tableName}...`);
-        linesReceived = true;
+  return new Transform({
+    objectMode: true,
+    transform(chunk, encoding, callback) {
+      if (!chunk) {
+        // line === null marks the end of the file. End the import stream
+        // to flush any items left in the collect buffer.
+        return callback(null, null);
       }
 
-      try {
-        // Parse the line and return it into the stream
-        const parsedLine = parseLine(line, lineSchema);
-        const notAllowedNulls = hasProhibitedNulls(parsedLine, lineSchema);
-        if (!notAllowedNulls) {
-          return cb(null, parsedLine);
+      if (lineSchema) {
+        // This function runs on each line which would be too much to log.
+        // When receiving the first line of a table, log it and mark it as logged.
+        if (!linesReceived) {
+          console.log(`Importing ${tableName}...`);
+          linesReceived = true;
         }
-      } catch (err) {
-        return cb(err);
-      }
-    }
 
-    return cb();
+        try {
+          // Parse the line and return it into the stream
+          const parsedLine = parseLine(chunk, lineSchema);
+          const notAllowedNulls = hasProhibitedNulls(parsedLine, lineSchema);
+          if (!notAllowedNulls) {
+            return callback(null, parsedLine);
+          }
+        } catch (err) {
+          return callback(err);
+        }
+      }
+
+      return callback();
+    },
   });
 };
 
